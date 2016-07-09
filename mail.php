c1c69b9085c047a9c5def0e4f20f1fa5fd49297c
jbravot <jjbravo88@gmail.com>
2016-07-09 08:50:00 -0500

    [ADD] agregando php mailer

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
 //incluimos la clase PHPMailer
-require_once('lib/PHPMailer/class.phpmailer.php');
+require_once("lib/PHPMailer/PHPMailerAutoload.php");
 
 //instancio un objeto de la clase PHPMailer
 $mail = new PHPMailer(); // defaults to using php "mail()"
 
-$mail->IsSMTP(); // telling the class to use SMTP
+$mail->isSMTP(); // telling the class to use SMTP
 $mail->SMTPAuth   = true;                  // enable SMTP authentication
 $mail->SMTPSecure = "tls";                 // sets the prefix to the servier
 $mail->Host       = "smtp.gmail.com";      // sets GMAIL as the SMTP server
@@ -14,35 +14,37 @@ $mail->Username   = "jjbravo88@gmail.com";  // GMAIL username
 $mail->Password   = "tarpuyJON963";            // GMAIL password
 
 //defino el email y nombre del remitente del mensaje
-$mail­>SetFrom('jjbravo88@gmail.com', 'RUMI');
-$mail­>AddAddress('jjbravo88@gmail.com', "Jonathan Bravo");
+///$mail->setFrom("jjbravo88@gmail.com", "RUMI");
+$mail->From = 'jjbravo88@gmail.com';
+$mail->FromName = 'RUMI';
+$mail->AddAddress("jjbravo88@gmail.com", "Jonathan Bravo");
 
-$elmensaje = str_replace("\n.", "\n..", $_POST['text']);     //por si el mensaje empieza con un punto ponerle 2
+$elmensaje = str_replace("\n.", "\n..", $_POST["text"]);     //por si el mensaje empieza con un punto ponerle 2
 $elmensaje = wordwrap($elmensaje, 70);                       //dividir el mensaje en trozos de 70 cols
-$cuerpomsg ='
+$cuerpomsg ="
 	<html>
 	<head>
 	  <title>Tienes un mensaje!!</title>
 	</head>
 	<body>
-	<p>jonathan Bravo, '.$_POST['name'].' - '.$_POST['email'].' te ha enviado un mensaje desde el sitio web http://www.rumiec.com</p>
+	<p>jonathan Bravo, ".$_POST["name"]." - ".$_POST["email"]." te ha enviado un mensaje desde el sitio web http://www.rumiec.com</p>
 	  <table>
 		<tr>
 		  <td><b>Tu mensaje es:</b><br></td>
 		</tr>
 		<tr>
-		  <td>'.$elmensaje.'</td>
+		  <td>".$elmensaje."</td>
 		</tr>
 	  </table>
 	</body>
 	</html>
-';
+";
 
-/Añado un asunto al mensaje
-$mail­>Subject = "[RUMI][ACADEMICO] Mensaje desde sitio web";
-$mail­>MsgHTML($cuerpomsg);
+//Añado un asunto al mensaje
+$mail->Subject = "[RUMI][ACADEMICO] Mensaje desde sitio web";
+$mail->MsgHTML($cuerpomsg);
 
-if(!$mail­>Send()) {
+if(!$mail->Send()) {
 	echo "0";
 } else {
 	echo "1";
